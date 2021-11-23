8a773ea93461194afb4ff024b3e932bf1031873b
maricm123 <mihailomaric001@gmail.com>
2021-11-23 10:53:23 +0100

    setting firebase

---
+++ nursinghome/src/firebase/firebase.js
@@ -0,0 +1,16 @@
+import firebase from 'firebase/compat/app'
+
+
+const firebaseConfig = {
+    apiKey: "AIzaSyB3ugsZdTkCkWJ2gfk8MrkmnKlQqgHD5cA",
+    authDomain: "nursinghome-8dfbb.firebaseapp.com",
+    projectId: "nursinghome-8dfbb",
+    storageBucket: "nursinghome-8dfbb.appspot.com",
+    messagingSenderId: "950201848724",
+    appId: "1:950201848724:web:851bb6580b97d76f137581",
+    measurementId: "G-LGTS5L7ZE4"
+  };
+  
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+  firebase.initializeApp(firebaseConfig);
